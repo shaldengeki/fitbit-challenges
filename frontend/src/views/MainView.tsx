6b1f43d820fcac23986bbe6f18f0b40358d0f093
Charles Guo <shaldengeki@gmail.com>
2023-04-13 00:49:51 -0400

    Move view out to MainView

---
+++ frontend/src/views/MainView.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const TEST_QUERY = gql`
+  query Test {
+    test
+  }
+`;
+
+function MainView() {
+  const { loading, error, data } = useQuery(TEST_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div>{data.test}</div>
+  )
+}
+
+export default MainView;
