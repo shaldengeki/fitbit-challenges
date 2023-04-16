a110c0e7dbb0e40478743972ff1f0dd512f3b6fe
Charles Guo <shaldengeki@gmail.com>
2023-04-16 13:21:46 -0400

    Run black

--- api/tests/test_example.py
+++ api/tests/test_example.py
@@ -7,22 +7,28 @@ from typing import Iterator
 
 from ..app import app as base_app
 
+
 @pytest.fixture
 def app() -> Iterator[flask.Flask]:
-    base_app.config.update({
-        "TESTING": True,
-    })
+    base_app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
     yield base_app
+
 
 @pytest.fixture
 def client(app: flask.Flask) -> FlaskClient:
     return app.test_client()
 
+
 @pytest.fixture
 def runner(app: flask.Flask) -> FlaskCliRunner:
     return app.test_cli_runner()
 
+
 def test_example_request(client: FlaskClient) -> None:
     response = client.post("/graphql", json={"query": "query {\n  test\n}"})
     response_data = json.loads(response.data)
-    assert response_data['data']['test'] == "hello world!"
+    assert response_data["data"]["test"] == "hello world!"
