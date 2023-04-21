f22c6d65904de311581443018a33baa6820d98fc
Charles Guo <shaldengeki@gmail.com>
2023-04-21 01:46:27 -0400

    Remove dark-mode style

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -12,7 +12,7 @@ const ProgressBar = ({ value, maximum }: ProgressBarProps) => {
     };
     return (
         <div className="mb-6 h-7 w-full bg-teal-400 text-right">
-            <div className="h-7 bg-neutral-200 dark:bg-neutral-600" style={barStyles}>
+            <div className="h-7 bg-neutral-200" style={barStyles}>
                 {value}
             </div>
         </div>
