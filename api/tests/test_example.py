d63b1190d59e22779d60474e4b58970d1bec49d8
Charles Guo <shaldengeki@gmail.com>
2023-05-17 23:17:59 -0400

    Disable example test

--- api/tests/test_example.py
+++ api/tests/test_example.py
@@ -3,15 +3,15 @@ from flask.testing import FlaskClient
 from .fixtures import app, client  # noqa
 
 
-def test_example_request(client: FlaskClient) -> None:
-    response = client.post(
-        "/graphql",
-        json={
-            "query": """
-                query {
-                    test
-                }
-            """
-        },
-    )
-    assert response.json["data"]["test"] == "hello world!"
+# def test_example_request(client: FlaskClient) -> None:
+#     response = client.post(
+#         "/graphql",
+#         json={
+#             "query": """
+#                 query {
+#                     test
+#                 }
+#             """
+#         },
+#     )
+#     assert response.json["data"]["test"] == "hello world!"
