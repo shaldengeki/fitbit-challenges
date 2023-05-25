612bd509672daf7ed3f8de6e35bf90968902ad1c
Charles OuGuo <shaldengeki@gmail.com>
2023-05-25 10:39:42 -0400

    Move signin link to right

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -9,10 +9,12 @@ type NavBarProps = {
 const NavBar = (props: NavBarProps) => {
     return (
         <div className="leading-6 w-full sticky border-b border-b-slate-50 py-4">
-            <Link to={'/auth'}>
-                <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
-                <span>Sign in with Fitbit</span>
-            </Link>
+            <div className="relative flex items-center">
+                <Link to={'/auth'} className="ml-auto">
+                    <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
+                    <span>Sign in with Fitbit</span>
+                </Link>
+            </div>
         </div>
     );
 };
