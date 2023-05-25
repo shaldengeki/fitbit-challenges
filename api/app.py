770982ed1e0480fb6d76976d1490b07e54b9b3d7
Charles OuGuo <shaldengeki@gmail.com>
2023-05-25 01:17:44 -0400

    Add subscription notification table, and a REST endpoint that verifies fitbit secret

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
+from flask import abort, request
 from graphql_server.flask import GraphQLView
-from .config import app
+from .config import app, verify_fitbit_signature
 from . import models
 
 from . import gql
@@ -8,3 +9,13 @@ app.add_url_rule(
     "/graphql",
     view_func=GraphQLView.as_view("graphql", schema=gql.Schema(models), graphiql=True),
 )
+
+
+@app.route("/fitbit-notifications", methods=["POST"])
+def fitbit_notifications():
+    if not verify_fitbit_signature(
+        request.headers.get("X-Fitbit-Signature", ""), request.get_data()
+    ):
+        abort(400)
+
+    return "yay!"
