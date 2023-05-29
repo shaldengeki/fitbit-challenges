2179ba04492ac8be468f0eee0092220f64bab8b7
Charles OuGuo <shaldengeki@gmail.com>
2023-05-29 01:39:37 -0400

    Correctly refresh token; also mark older requests as done

--- src/python/worker/app.py
+++ src/python/worker/app.py
@@ -1,8 +1,12 @@
+from base64 import b64encode
+import datetime
+from datetime import timezone
 import requests
 from sqlalchemy import desc, update
 from sqlalchemy.sql.functions import now
 import time
 from typing import Optional
+from urllib.parse import urlencode
 
 from ..config import app, db
 from ..models import SubscriptionNotification, User, UserActivity
@@ -12,7 +16,7 @@ def maybe_fetch_subscription_notification() -> Optional[SubscriptionNotification
     # Lock a job, if it exists.
     notification = (
         SubscriptionNotification.query.filter(
-            SubscriptionNotification.processed_at is None
+            SubscriptionNotification.processed_at == None
         )
         .order_by(desc(SubscriptionNotification.created_at))
         .first()
@@ -22,7 +26,7 @@ def maybe_fetch_subscription_notification() -> Optional[SubscriptionNotification
         return None
 
     print(f"Subscription notification to process: {notification.id}")
-    notification.processed_at = now
+    notification.processed_at = datetime.datetime.now().astimezone(timezone.utc)
     db.session.add(notification)
     db.session.commit()
     print(f"Notification locked.")
@@ -36,8 +40,49 @@ def fetch_user_for_notification(
     return User.query.filter(User.fitbit_user_id == notification.fitbit_user_id).first()
 
 
+def request_indicates_expired_token(response: dict) -> bool:
+    return "errors" in response and any(
+        e["errorType"] == "expired_token" for e in response["errors"]
+    )
+
+
+def refresh_tokens_for_user(user: User, client_id: str, client_secret: str) -> User:
+    encoded_client_and_secret = b64encode(
+        f"{client_id}:{client_secret}".encode("utf-8")
+    ).decode("utf-8")
+
+    url_parameters = urlencode(
+        {
+            "grant_type": "refresh_token",
+            "refresh_token": user.fitbit_refresh_token,
+        }
+    )
+
+    response = requests.post(
+        "https://api.fitbit.com/oauth2/token",
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {encoded_client_and_secret}",
+        },
+        data=url_parameters,
+    )
+
+    if response.status_code not in (200, 201):
+        raise ValueError(f"Error when refreshing user tokens: {response.json()}")
+
+    data = response.json()
+    user.fitbit_access_token = data["access_token"]
+    user.fitbit_refresh_token = data["refresh_token"]
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
 def fetch_user_activity_for_notification(
     notification: SubscriptionNotification,
+    client_id: str,
+    client_secret: str,
 ) -> dict:
     formatted_date: str = notification.date.strftime("%Y-%m-%d")
     user = fetch_user_for_notification(notification)
@@ -49,11 +94,22 @@ def fetch_user_activity_for_notification(
         f"https://api.fitbit.com/1/user/{notification.fitbit_user_id}/activities/date/{formatted_date}.json",
         headers={"Authorization": f"Bearer {user.fitbit_access_token}"},
     ).json()
-    print(f"Fetched data: {data}")
+
+    if request_indicates_expired_token(data):
+        print(f"Refreshing expired token.")
+        user = refresh_tokens_for_user(user, client_id, client_secret)
+        print(
+            f"Fetching {notification.fitbit_user_id}'s activity for {formatted_date}."
+        )
+        data = requests.get(
+            f"https://api.fitbit.com/1/user/{notification.fitbit_user_id}/activities/date/{formatted_date}.json",
+            headers={"Authorization": f"Bearer {user.fitbit_access_token}"},
+        ).json()
+
     return data
 
 
-def process_subscription_notifications() -> None:
+def process_subscription_notifications(client_id: str, client_secret: str) -> None:
     notification = maybe_fetch_subscription_notification()
     if notification is None:
         print("No subscription notifications to process, skipping.")
@@ -61,7 +117,9 @@ def process_subscription_notifications() -> None:
 
     try:
         # Fetch the user's activity for this date.
-        activity = fetch_user_activity_for_notification(notification)
+        activity = fetch_user_activity_for_notification(
+            notification, client_id, client_secret
+        )
         active_minutes: int = (
             activity["summary"]["veryActiveMinutes"]
             + activity["summary"]["fairlyActiveMinutes"]
@@ -83,7 +141,7 @@ def process_subscription_notifications() -> None:
             distance_km=distance_km,
         )
 
-        print(f"Recording new activity: {new_activity}")
+        print(f"Recording new activity.")
         db.session.add(new_activity)
         db.session.commit()
     except:
@@ -92,15 +150,28 @@ def process_subscription_notifications() -> None:
         db.session.commit()
         raise
 
+    # Mark all older notifications than this one as done, too.
+    update_older_notifications = (
+        update(SubscriptionNotification)
+        .where(SubscriptionNotification.fitbit_user_id == notification.fitbit_user_id)
+        .where(SubscriptionNotification.date == notification.date)
+        .where(SubscriptionNotification.created_at <= notification.created_at)
+        .values(processed_at=datetime.datetime.now().astimezone(timezone.utc))
+    )
+    db.session.execute(update_older_notifications)
+
 
 max_delay = 10
 
 
 def main() -> int:
     with app.app_context():
+        client_id = app.config["FITBIT_CLIENT_ID"]
+        client_secret = app.config["FITBIT_CLIENT_SECRET"]
+
         while True:
             start = time.time()
-            process_subscription_notifications()
+            process_subscription_notifications(client_id, client_secret)
             delay = (time.time() + max_delay) - start
             if delay > 0:
                 print(f"Sleeping for {round(delay)} seconds")
