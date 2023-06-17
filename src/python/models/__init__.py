0735a7379da6b02f4815b11e3ab745f66e88a815
Charles OuGuo <shaldengeki@gmail.com>
2023-06-17 04:48:23 -0400

    WIP: Stage work

--- src/python/models/__init__.py
+++ src/python/models/__init__.py
@@ -1,4 +1,4 @@
 from .challenge import Challenge
-from .user import User
+from .user import User, FitbitSubscription
 from .user_activity import UserActivity
 from .subscription_notification import SubscriptionNotification
--- src/python/models/user.py
+++ src/python/models/user.py
@@ -1,7 +1,11 @@
-from ..config import db
 import datetime
 import requests
+import sqlalchemy
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import Optional
+
+from ..config import db
 
 
 class User(db.Model):  # type: ignore
@@ -14,10 +18,21 @@ class User(db.Model):  # type: ignore
     fitbit_access_token = db.Column(db.Unicode(500), nullable=False)
     fitbit_refresh_token = db.Column(db.Unicode(100), nullable=False)
     synced_at = db.Column(db.TIMESTAMP(timezone=True), nullable=True)
-    fitbit_subscription_id = db.Column(db.Identity(), nullable=False)
 
     def __repr__(self) -> str:
         return "<User {fitbit_user_id}>".format(fitbit_user_id=self.fitbit_user_id)
+
+    @property
+    def fitbit_subscription(self) -> "FitbitSubscription":
+        sub = FitbitSubscription.query.filter(
+            FitbitSubscription.fitbit_user_id == self.fitbit_user_id
+        ).first
+        if sub is None:
+            raise ValueError(
+                f"Subscription expected but not found for user {self.fitbit_user_id}"
+            )
+
+        return sub
 
     @staticmethod
     def fetch_display_name_with_user_id_and_access_token(
@@ -61,7 +76,7 @@ class User(db.Model):  # type: ignore
             )
         )
 
-    def create_subscription(self) -> requests.Response:
+    def create_subscription(self) -> Optional["FitbitSubscription"]:
         sub_request = requests.post(
             f"https://api.fitbit.com/1/user/{self.fitbit_user_id}/activities/apiSubscriptions/{self.fitbit_subscription_id}.json",
             headers={
@@ -73,4 +88,26 @@ class User(db.Model):  # type: ignore
 
         if sub_request.status_code not in (200, 201):
             actual_subscription_id = sub_request.json()["subscriptionId"]
-            self.fitbit_subscription_id = actual_subscription_id
+            return None
+
+        return FitbitSubscription(fitbit_user_id=self.fitbit_user_id)
+
+
+class FitbitSubscription(db.Model):  # type: ignore
+    __tablename__ = "fitbit_subscriptions"
+    id = db.Column(db.Integer, primary_key=True)
+    fitbit_user_id = db.Column(db.Unicode(100), nullable=False)
+
+    def __repr__(self) -> str:
+        return "<FitbitSubscription {fitbit_user_id}>".format(
+            fitbit_user_id=self.fitbit_user_id
+        )
+
+    @property
+    def user(self) -> "User":
+        u = User.query.filter(User.fitbit_user_id == self.fitbit_user_id).first
+        if u is None:
+            raise ValueError(
+                f"User expected but none found for fitbit subscription {self.fitbit_user_id}"
+            )
+        return u
