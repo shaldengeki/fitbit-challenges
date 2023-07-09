e5699706aa1066fa39f0f2cc7c1f3e2ae771272b
Charles Guo <shaldengeki@gmail.com>
2023-07-09 01:07:53 -0400

    Confine navbar width, and fix light/darkmode styles

--- src/javascript/frontend/src/components/NavBar.tsx
+++ src/javascript/frontend/src/components/NavBar.tsx
@@ -25,30 +25,32 @@ const NavBar = (props: NavBarProps) => {
     );
 
     return (
-        <div className="leading-6 w-full sticky border-b border-b-slate-50 py-4 px-4 bg-blue-200 dark:bg-slate-900 dark:text-slate-400">
-            <div className="relative flex items-center gap-2">
-                <img className="flex-none h-6 inline" src={logo192} alt="Fitbit app icon" />
-                <div className="dark:hover:text-slate-300 hover:text-slate-500">
-                    <Link to={'/challenges'}>
-                        <p className="font-bold">Challenges</p>
-                    </Link>
+        <div className="sticky top-0 z-40 w-full backdrop-blur flex-none lg:border-b lg:border-slate-900/10 lg:dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+            <div className="max-w-screen-2xl mx-auto">
+                <div className="py-4 mx-4 lg:mx-0 lg:px-8 relative flex items-center gap-2 dark:text-slate-400">
+                    <img className="flex-none h-6 inline" src={logo192} alt="Fitbit app icon" />
+                    <div className="dark:hover:text-slate-300 hover:text-slate-500">
+                        <Link to={'/challenges'}>
+                            <p className="font-bold">Challenges</p>
+                        </Link>
+                    </div>
+                    {
+                        loading && <p>Loading...</p>
+                    }
+                    {
+                        error && <p>Error loading login state</p>
+                    }
+                    {
+                        data && data.currentUser === null &&
+                        <Link to={'/auth'} className="ml-auto dark:hover:text-slate-300 hover:text-slate-500">
+                            <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
+                            <span className="font-bold">Sign in with Fitbit</span>
+                        </Link>
+                    }
+                    {
+                        data && data.currentUser && <p className="ml-auto">{data.currentUser.displayName}</p>
+                    }
                 </div>
-                {
-                    loading && <p>Loading...</p>
-                }
-                {
-                    error && <p>Error loading login state</p>
-                }
-                {
-                    data && data.currentUser === null &&
-                    <Link to={'/auth'} className="ml-auto dark:hover:text-slate-300 hover:text-slate-500">
-                        <img className="h-5 inline" src={fitbit} alt="Fitbit app icon" />
-                        <span className="font-bold">Sign in with Fitbit</span>
-                    </Link>
-                }
-                {
-                    data && data.currentUser && <p className="ml-auto">{data.currentUser.displayName}</p>
-                }
             </div>
         </div>
     );
